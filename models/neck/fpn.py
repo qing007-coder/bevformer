1c2f81e7a718596e724d0d13e9f52bda07af7cfe
xubo <1457800511@qq.com>
2026-09-18 17:51:59 +0800

    feat: 添加FPN多尺度特征融合模块

---
+++ models/neck/fpn.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FPN(nn.Module):
+
+    def __init__(self, out_channels=256):
+        super().__init__()
+
+        self.lateral_c2 = nn.Conv2d(256, out_channels, kernel_size=1)
+        self.lateral_c3 = nn.Conv2d(512, out_channels, kernel_size=1)
+        self.lateral_c4 = nn.Conv2d(1024, out_channels, kernel_size=1)
+        self.lateral_c5 = nn.Conv2d(2048, out_channels, kernel_size=1)
+
+        self.smooth_p5 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.smooth_p4 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.smooth_p3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.smooth_p2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, c2, c3, c4, c5):
+        p5 = self.lateral_c5(c5)
+        p5 = self.smooth_p5(p5)
+
+        p4 = self.lateral_c4(c4) + F.interpolate(p5, size=c4.shape[-2:], mode='nearest')
+        p4 = self.smooth_p4(p4)
+
+        p3 = self.lateral_c3(c3) + F.interpolate(p4, size=c3.shape[-2:], mode='nearest')
+        p3 = self.smooth_p3(p3)
+
+        p2 = self.lateral_c2(c2) + F.interpolate(p3, size=c2.shape[-2:], mode='nearest')
+        p2 = self.smooth_p2(p2)
+        
+        return p2, p3, p4, p5
